31286ba332a610ec61feaeed3c779c461e97c549
Fern Sanchez <me@fjorn.dev>
2025-09-01 02:38:19 -0400

    FAS - blog posts and nice math rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,33 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
-
+import remarkMath from "remark-math";
+import remarkToc from "remark-toc";
+import rehypeKatex from "rehype-katex";
 import vercel from "@astrojs/vercel";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react()],
+  markdown: {
+    remarkPlugins: [remarkMath, [remarkToc, { heading: "Contents" }]],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: "wrap" }],
+    ],
+    shikiConfig: {
+      themes: {
+        light: "catppuccin-latte",
+        dark: "catppuccin-macchiato",
+      },
+    },
+  },
+  integrations: [react(), mdx()],
   adapter: vercel(),
 });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDate(dateString: string | Date): string {
+  const date =
+    typeof dateString === "string" ? new Date(dateString) : dateString;
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+  };
+  return date.toLocaleDateString("en-US", options);
+}
